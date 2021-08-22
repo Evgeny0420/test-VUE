e0c5d89db62c7c27a54164ed1c27ab4bead5950b
Евгений Карпов <evgeniy_karpov91@inbox.ru>
2021-08-22 23:03:25 +0300

    Добавляет данные

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,129 +1,87 @@
 <template>
-    <div>
-      <ul class="goods">
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-        <li class="goods__item">
-          <div class="goods__photo">
-            <img src="@/assets/img/rectangle-30.jpg" alt="Photo" class="goods-photo">
-          </div>
-          <div class="goods__about">
-            <h3 class="goods__title">Наименование товара</h3>
-            <p class="goods__about-text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в
-              несколько строк</p>
-            <p class="goods__prise">10 000 руб.</p>
-          </div>
-        </li>
-      </ul>
-    </div>
+  <div>
+    <ul class="goods">
+      <li class="goods__item" v-for="good in goods" :key="good.id">
+        <div>
+          <img :src="good.link" alt="Photo" class="goods-photo">
+        </div>
+        <div class="goods__about">
+          <h3 class="goods__title">{{ good.title }}</h3>
+          <p class="goods__about-text">{{ good.aboutText }}</p>
+          <p class="goods__price">{{ good.price }} руб.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Goods'
-    };
+export default {
+  name: 'Goods',
+  data() {
+    return {
+      goods: [
+        {
+          id: 1,
+          title: `Наименование товара`,
+          aboutText: `Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк`,
+          link: "./assets/img/rectangle-30.jpg",
+          price: 10000,
+        },
+      ]
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  .goods {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 16px;
-  }
-  .goods__item {
-    width: 332px;
-    min-height: 423px;
-    background: #FFFEFB;
-    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
-    border-radius: 4px;
+.goods {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 16px;
+}
 
-    &:hover {
-      filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25)) drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25));
-    }
-  }
+.goods__item {
+  width: 332px;
+  min-height: 423px;
+  background: #FFFEFB;
+  box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
 
-  .goods__photo {
-    width: 100%;
+  &:hover {
+    filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25)) drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25));
   }
+}
 
-  .goods__about {
-    padding: 16px;
-  }
+.goods__photo {
+  width: 100%;
+}
 
-  .goods__title {
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 25px;
-    margin: 0 0 16px;
-  }
+.goods__about {
+  padding: 16px;
+}
 
-  .goods__about-text {
-    font-weight: normal;
-    font-size: 16px;
-    line-height: 20px;
-    margin: 0 0 32px;
-  }
+.goods__title {
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 25px;
+  margin: 0 0 16px;
+}
 
-  .goods__prise {
-    font-weight: 600;
-    font-size: 24px;
-    line-height: 30px;
-    margin: 0 0 8px;
-  }
+.goods__about-text {
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  margin: 0 0 32px;
+}
+
+.goods__price {
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+  margin: 0 0 8px;
+}
 </style>
